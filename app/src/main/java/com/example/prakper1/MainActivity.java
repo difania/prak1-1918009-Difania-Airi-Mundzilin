e63f797d6c4511c750e7bc9810b76a5b7a269272
unknown <difaniamundzilin@gmail.com>
2021-10-05 14:19:48 +0700

    first commit

---
+++ app/src/main/java/com/example/prakper1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.prakper1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void biodata(View view){
+        Intent a = new Intent(MainActivity.this, Biodata.class);
+        startActivity(a);
+    }
+}
